2280deb654918827131af9dbdfd13a77824f7646
Nanda Mochammad <nanda.mocha@gmail.com>
2025-07-29 16:04:47 +0700

    Initial Commit

---
+++ SwiftUI-IntermediateLearning/SwiftUI_IntermediateLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_IntermediateLearningApp.swift
+//  SwiftUI-IntermediateLearning
+//
+//  Created by Nanda Mochammad on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_IntermediateLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
