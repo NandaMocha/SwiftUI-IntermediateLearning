9d8b749dd13bd8170c214871a1f06239ed6a763d
Nanda Mochammad <nanda.mocha@gmail.com>
2025-07-29 20:50:06 +0700

    Rotation Gesture https://www.youtube.com/watch?v=vSvhdwy_8Lk

---
+++ SwiftUI-IntermediateLearning/Learning/RotationGesture.swift
@@ -0,0 +1,39 @@
+//
+//  RotationGesture.swift
+//  SwiftUI-IntermediateLearning
+//
+//  Created by Nanda Mochammad on 29/07/25.
+//
+
+import SwiftUI
+
+struct RotationGesture: View {
+    @GestureState private var rotateValue = 0.0
+    @State private var angleValue: Angle = Angle(degrees: 0)
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .padding()
+            .foregroundStyle(.white)
+            .padding(50)
+            .background(Color.blue.clipShape(.rect(cornerRadius: 10)))
+            .rotationEffect(angleValue)
+            .gesture(
+                RotateGesture()
+//                    .updating($rotateValue, body: { value, state, transaction in
+//                        state = value.rotation.degrees
+//                    })
+                    .onChanged({ value in
+                        angleValue = value.rotation
+                    })
+                    .onEnded({ _ in
+                        angleValue = Angle(degrees: 0)
+                    })
+            )
+        
+    }
+}
+
+#Preview {
+    RotationGesture()
+}
