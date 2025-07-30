7f47600777769673d0ec53c1dd92062677db3012
Nanda Mochammad <nanda.mocha@gmail.com>
2025-07-30 11:06:47 +0700

    Create mask with case study of star rating https://www.youtube.com/watch?v=pxx1ueCbnls

---
+++ SwiftUI-IntermediateLearning/Learning/MaskMaterials.swift
@@ -0,0 +1,47 @@
+//
+//  MaskMaterials.swift
+//  SwiftUI-IntermediateLearning
+//
+//  Created by Nanda Mochammad on 30/07/25.
+//
+
+import SwiftUI
+
+struct MaskMaterials: View {
+    @State var rating: Int = 0
+    var body: some View {
+        itemTomask
+            .overlay { overlayView.mask(itemTomask)}
+    }
+    
+    private var itemTomask: some View {
+        HStack {
+            ForEach(1..<6) { index in
+                Image(systemName: "star.fill")
+                    .font(.title)
+                    .foregroundStyle(.gray)
+                    .onTapGesture {
+                        withAnimation(.easeInOut) {
+                            rating = index
+                        }
+                    }
+            }
+        }
+    }
+    
+    private var overlayView: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .foregroundStyle(.blue)
+                    .frame(width: CGFloat(rating) / 5 * geometry.size.width)
+                
+            }
+        }
+        .allowsHitTesting(false)
+    }
+}
+
+#Preview {
+    MaskMaterials()
+}
