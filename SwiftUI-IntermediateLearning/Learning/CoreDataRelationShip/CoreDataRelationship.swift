d4e0d30173a929ac8068dbf1885f9f742b9ee371
Nanda Mochammad <nanda.mocha@gmail.com>
2025-08-03 20:32:51 +0700

    Add to Core Data

--- SwiftUI-IntermediateLearning/Learning/CoreDataRelationShip/CoreDataRelationship.swift
+++ SwiftUI-IntermediateLearning/Learning/CoreDataRelationShip/CoreDataRelationship.swift
@@ -39,14 +39,51 @@ class CoreDataRelationshipViewModel: ObservableObject {
     let manager = CoreDataRelationshipManager.instance
     
     @Published var businesses: [BusinessEntity] = []
+    @Published var departments: [DepartmentEntity] = []
+    @Published var employees: [EmployeeEntity] = []
     
     init() {
         getBusinesses()
+        getDepartments()
+        getEmployee()
     }
     
     func addBusiness() {
         let newBusiness = BusinessEntity(context: manager.context)
         newBusiness.name = "Apple Inc."
+        
+        // Add existing departments to the new business
+        //newBusiness.departments = []
+        
+        // Add existing employees to the new business
+        //newBusiness.employees = []
+        
+        // Add new business to existing department
+        //newBusiness.addToDepartments(<#T##value: DepartmentEntity##DepartmentEntity#>)
+        
+        // Add new business to existing employee
+        //newBusiness.addToEmployees(<#T##value: EmployeeEntity##EmployeeEntity#>)
+        
+        save()
+    }
+    
+    func addDepartment() {
+        let newDepartment = DepartmentEntity(context: manager.context)
+        newDepartment.name = "Marketing"
+        newDepartment.businesses = [businesses[0]]
+        
+        save()
+    }
+    
+    func addEmployee() {
+        let newEmployee = EmployeeEntity(context: manager.context)
+        newEmployee.age = 25
+        newEmployee.dateJoined = Date()
+        newEmployee.name = "Nanda"
+        
+        newEmployee.business = businesses[0]
+        newEmployee.department = departments[0]
+        
         save()
     }
     
@@ -59,8 +96,35 @@ class CoreDataRelationshipViewModel: ObservableObject {
         }
     }
     
+    func getDepartments() {
+        let request = NSFetchRequest<DepartmentEntity>(entityName: "DepartmentEntity")
+        do {
+            departments = try manager.context.fetch(request)
+        } catch let error {
+            print("Error getDepartments: ", error.localizedDescription)
+        }
+    }
+    
+    func getEmployee() {
+        let request = NSFetchRequest<EmployeeEntity>(entityName: "EmployeeEntity")
+        do {
+            employees = try manager.context.fetch(request)
+        } catch let error {
+            print("Error getEmployee: ", error.localizedDescription)
+        }
+    }
+    
     func save() {
-        manager.saveData()
+        businesses.removeAll()
+        departments.removeAll()
+        employees.removeAll()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.manager.saveData()
+            self.getBusinesses()
+            self.getDepartments()
+            self.getEmployee()
+        }
     }
 }
 
@@ -72,7 +136,7 @@ struct CoreDataRelationship: View {
             ScrollView {
                 VStack(spacing: 20) {
                     Button {
-                        viewModel.addBusiness()
+                        viewModel.addEmployee()
                     } label: {
                         Text("Perform Action")
                             .foregroundStyle(.white)
@@ -86,6 +150,22 @@ struct CoreDataRelationship: View {
                         HStack(alignment: .top) {
                             ForEach(viewModel.businesses) { business in
                                 BusinessView(entity: business)
+                            }
+                        }
+                    }
+                    
+                    ScrollView(.horizontal, showsIndicators: true) {
+                        HStack(alignment: .top) {
+                            ForEach(viewModel.departments) { department in
+                                DepartmentView(entity: department)
+                            }
+                        }
+                    }
+                    
+                    ScrollView(.horizontal, showsIndicators: true) {
+                        HStack(alignment: .top) {
+                            ForEach(viewModel.employees) { employee in
+                                EmployeeView(entity: employee)
                             }
                         }
                     }
@@ -124,6 +204,64 @@ struct BusinessView: View {
         .padding()
         .frame(maxWidth: 300, alignment: .leading)
         .background(.gray.opacity(0.5))
+        .clipShape(.rect(cornerRadius: 10))
+        .shadow(radius: 10)
+    }
+}
+
+struct DepartmentView: View {
+    let entity: DepartmentEntity
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            Text("Name: \(entity.name ?? "")")
+                .bold()
+            
+            if let businesses = entity.businesses?.allObjects as? [BusinessEntity] {
+                Text("Business:")
+                    .bold()
+                ForEach(businesses) { business in
+                    Text(business.name ?? "")
+                }
+            }
+            
+            if let employees = entity.employees?.allObjects as? [EmployeeEntity] {
+                Text("Employees:")
+                    .bold()
+                ForEach(employees) { employee in
+                    Text(employee.name ?? "")
+                }
+            }
+        }
+        .padding()
+        .frame(maxWidth: 300, alignment: .leading)
+        .background(.green.opacity(0.5))
+        .clipShape(.rect(cornerRadius: 10))
+        .shadow(radius: 10)
+    }
+}
+
+struct EmployeeView: View {
+    let entity: EmployeeEntity
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            Text("Name: \(entity.name ?? "")")
+                .bold()
+            Text("Age: \(entity.age)")
+            Text("Date Joined: \(entity.dateJoined ?? Date())")
+            
+            Text("Business:")
+                .bold()
+            Text("\(entity.business?.name ?? "")")
+            
+            Text("Department:")
+                .bold()
+            Text("\(entity.department?.name ?? "")")
+        }
+        .padding()
+        .frame(maxWidth: 300, alignment: .leading)
+        .background(.orange.opacity(0.5))
         .clipShape(.rect(cornerRadius: 10))
         .shadow(radius: 10)
     }
